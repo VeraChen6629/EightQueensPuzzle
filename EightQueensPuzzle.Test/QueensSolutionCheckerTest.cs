5efcf371b62107aa3987f04f20802e9302dbf599
vera6629 <vera6629@gmail.com>
2021-01-30 16:34:14 +0800

    update unit test code

--- EightQueensPuzzle.Test/QueensSolutionCheckerTest.cs
+++ EightQueensPuzzle.Test/QueensSolutionCheckerTest.cs
@@ -5,24 +5,24 @@ namespace EightQueensPuzzle.Test
     [TestClass]
     public class QueensSolutionCheckerTest
     {
-        QueensSolutionChecker qsChecker;
+        private QueensSolutionChecker _qsChecker;
 
         [TestMethod]
         public void TotalNQueensTest()
         {
-            qsChecker = new QueensSolutionChecker();
-            Assert.AreEqual(1, qsChecker.GetQueensSolutions(1,false));
-            Assert.AreEqual(0, qsChecker.GetQueensSolutions(2,false));
-            Assert.AreEqual(0, qsChecker.GetQueensSolutions(3,false));
-            Assert.AreEqual(2, qsChecker.GetQueensSolutions(4,false));
-            Assert.AreEqual(10, qsChecker.GetQueensSolutions(5,false));
-            Assert.AreEqual(4, qsChecker.GetQueensSolutions(6,false));
-            Assert.AreEqual(40, qsChecker.GetQueensSolutions(7,false));
-            Assert.AreEqual(92, qsChecker.GetQueensSolutions(8,false));
-            Assert.AreEqual(352, qsChecker.GetQueensSolutions(9,false));
-            Assert.AreEqual(724, qsChecker.GetQueensSolutions(10, false));
-            Assert.AreEqual(2680, qsChecker.GetQueensSolutions(11, false));
-            Assert.AreEqual(14200, qsChecker.GetQueensSolutions(12, false));
+            _qsChecker = new QueensSolutionChecker();
+            Assert.AreEqual(1, _qsChecker.GetQueensSolutions(1,false));
+            Assert.AreEqual(0, _qsChecker.GetQueensSolutions(2,false));
+            Assert.AreEqual(0, _qsChecker.GetQueensSolutions(3,false));
+            Assert.AreEqual(2, _qsChecker.GetQueensSolutions(4,false));
+            Assert.AreEqual(10, _qsChecker.GetQueensSolutions(5,false));
+            Assert.AreEqual(4, _qsChecker.GetQueensSolutions(6,false));
+            Assert.AreEqual(40, _qsChecker.GetQueensSolutions(7,false));
+            Assert.AreEqual(92, _qsChecker.GetQueensSolutions(8,false));
+            Assert.AreEqual(352, _qsChecker.GetQueensSolutions(9,false));
+            Assert.AreEqual(724, _qsChecker.GetQueensSolutions(10, false));
+            Assert.AreEqual(2680, _qsChecker.GetQueensSolutions(11, false));
+            Assert.AreEqual(14200, _qsChecker.GetQueensSolutions(12, false));
 
         }
     }
